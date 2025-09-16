5c068e7f5255ff49d70c6ea2cdb3a60a5e374efd
MoeHein <you@example.com>
2025-09-16 23:02:16 +0800

    upate form

--- src/Components/SearchList.tsx
+++ src/Components/SearchList.tsx
@@ -1,20 +1,24 @@
-import type { ChangeEvent } from "react";
+import type { ChangeEvent, FormEvent } from "react";
 
 type SearchProp = {
   filterUser: string;
   handleFilter: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
 };
 
-const SearchList = ({ filterUser, handleFilter }: SearchProp) => {
+const SearchList = ({ filterUser, handleFilter, handleSubmit }: SearchProp) => {
   return (
-    <div className="border-0 p-3 my-4">
-      <input
-        type="text"
-        placeholder="Search the user"
-        value={filterUser}
-        onChange={handleFilter}
-        className="border p-4 rounded-xl"
-      />
+    <div className="border-0 p-3 my-4 ">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search the user"
+          value={filterUser}
+          onChange={handleFilter}
+          className="border p-4 rounded-xl mr-4"
+        />
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 };
